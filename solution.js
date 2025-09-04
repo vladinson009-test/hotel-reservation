a6f5d7e127fac6b72e55b6a48a309ca9de00b39d
Vladimir Gulev <vladinson009@icloud.com>
2025-09-04 22:37:06 +0200

    Offerer job is done here

--- solution.js
+++ solution.js
@@ -1,23 +1,70 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
-}
+let reservation = {
+  startDate: null,
+  endDate: null,
+  guestsCount: 0,
+  roomType: null,
+  name: null,
+  phone: null,
+  email: null,
+};
 
 function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
+  document
+    .querySelectorAll('.custom-form')
+    .forEach((div) => div.classList.add('hidden'));
+  if (document.querySelector(`.${className}`) != null) {
     document.querySelector(`.${className}`).classList.remove('hidden');
-    }
+  }
 }
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+document
+  .querySelector('#new-reservation')
+  .addEventListener('click', (e) => cleanData(e));
 
 function cleanData(e) {
-    changeContent('search-form-content');
+  changeContent('search-form-content');
+}
+document
+  .querySelector('#search-back-btn')
+  .addEventListener('click', (e) => fillSearchForm(e));
+
+function fillSearchForm(e) {
+  e.preventDefault();
+  changeContent('search-form-content');
+  document.querySelector('#check-in').value = reservation.startDate;
+  document.querySelector('#check-out').value = reservation.endDate;
+  document.querySelector('#people').value = reservation.guestsCount;
+}
+
+document.querySelectorAll('.room-type').forEach((room) => {
+  room.addEventListener('click', (e) => selectRoomType(e));
+});
+
+function selectRoomType(e) {
+  let myTarget = undefined;
+  e.preventDefault;
+  if (e.target.querySelector('img') != null) {
+    myTarget = e.target;
+  } else {
+    myTarget = e.target.parentElement;
+  }
+  document
+    .querySelectorAll('.room-type')
+    .forEach((room) => room.classList.remove('selected-room'));
+  myTarget.classList.add('selected-room');
+}
+
+document
+  .querySelector('#search-next-btn')
+  .addEventListener('click', (e) => findRoom(e));
+
+function findRoom(e) {
+  e.preventDefault();
+  const roomInfo =
+    e.target.parentElement.parentElement.querySelector(
+      '.selected-room h4'
+    ).textContent;
+  reservation.roomType = roomInfo;
+  console.log(reservation);
+  changeContent('guest-details-form-content');
 }
